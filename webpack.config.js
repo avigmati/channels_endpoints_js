9920d61c964cde526bafe614b179362606707faf
Dmitry Novikov <avigmati@gmail.com>
2021-09-06 21:24:21 +0700

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-'use strict'
-
 const path = require('path')
 
 module.exports = {
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, "dist"),
         filename: 'bundle.js',
         library: 'channels_endpoints',
         libraryTarget: 'umd'
